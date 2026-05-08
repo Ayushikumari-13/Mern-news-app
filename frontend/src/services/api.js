347b494b12821468309cbe83c4b6d112c2464959
Delta Ayushi <ayushiayushikumari24@gmail.com>
2026-05-08 22:56:11 +0530

    Connect frontend with live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-news-app-ab69.onrender.com/api",
 });
 
 export default API;
